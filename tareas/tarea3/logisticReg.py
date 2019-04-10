fe7e8513cecc7f2900db61bdbc5c3f1ee2b7f24f
erickgrm <erick.grm@gmail.com>
2019-04-10 15:48:12 -0500

    update 10Ap 15.40

--- tareas/tarea3/logisticReg.py
+++ tareas/tarea3/logisticReg.py
@@ -8,54 +8,103 @@ from custom_routines import custom_routines as cr
 
 class logistic_regression:
     coefs = [] 
-    minerror = 10.0
     features = []
     responses = []
+    lambd = 0.0
 
-    #def __init__(self, features, responses): 
-    #        self.features = features
-    #        self.responses = responses
-
-    def fit(self, features, responses):
+    # Main routine to train the model
+    # @param features matrix of features
+    #        responses vector of corresponding responses
+    #        lambd factor for l2 regularization; if 0, no regularization is applied
+    # It is assume features has first column of ones, so the 
+    # intercept is estimated directly 
+    def fit(self, features, responses, lambd):
         self.features = features
         self.responses = responses
-        theta0 = np.zeros(len(features[0]))
+        self.lambd = lambd
+
+        # Fix params for gradient descend algorithm
+        theta0 = np.ones(len(features[0]))
         alpha = 0.1
-        error = 0.00001
-        opt = cr.gd_minimize(self.E, self.gdE, theta0, alpha, error)
-        self.coefs = opt[0]
-        self.minerror = opt[1]
+        error = 0.0001
+
+        # Find optimal vector of weights through the gradient descend algorithm
+        self.coefs = self.gd_minimize(self.gdE, theta0, alpha, error)
+
+
+    # Minimise a function by the gradient descend algorithm
+    # @param gdf the gradient function
+    #        theta0 starting point of the algorithm, default vector of 1s
+    #        alpha the learning rate, default 0.1
+    #        error the permissible error, use 10^-4 as defualt
+    def gd_minimize(self, gdf, theta0, alpha, error):
+        max_iter = 300 
+        theta_min = theta0
+        temp = theta_min
+        e = 1.0
+        iter = 0
+
+        # Find minimum iteratively by gradient descend
+        while error < e and iter < max_iter:
+            temp = theta_min
+            theta_min = theta_min - (alpha * gdf(theta_min))
+            iter +=1
+        return theta_min
+    
+    # The gradient of the loss function, general to accept l2 regularization with parameter lambd
+    def gdE(self, theta): 
+        dtheta = np.dot(np.transpose(self.features),self.p(theta)-self.responses)
+        return dtheta + 2 * self.lambd * np.dot(np.ones(len(theta)), theta)
+
+    # Gradient without considering penalization at all, the above one is more general
+    #def gdE(self, theta): 
+    #    return np.dot(np.transpose(self.features),self.p(theta)-self.responses)
 
     def sigmoid(self,z):
         return 1/(1 + np.exp(-z))
 
-    def pi(self, theta,x):
-        return self.sigmoid(np.dot(theta,x))
-
     def p(self, theta):
         arr = np.zeros(len(self.responses))
         for i in range(0,len(self.features)):
-            arr[i] = self.pi(theta,self.features[i])
+            arr[i] = self.sigmoid(np.dot(theta, self.features[i]))
         return arr
 
-    def gdE(self, theta): 
-        return np.dot(np.transpose(self.features),self.p(theta)-self.responses)
 
-    def E(self,theta):
-        e = 0.0
-        for i in range(0,len(self.responses)):
-            pi = self.pi(theta, self.features[i])
-            yi = self.responses[i]
-            e += yi * np.log(pi) + (1-yi) * np.log(1-pi)
-        return -e
-    
     # Return the estimated parameters
     def coef(self): return self.coefs
     
     def intercept(self): return self.coefs[0]
     
+
+    # Return the probabilities of an instance x
+    def proba(self, x):
+        prob = self.sigmoid(np.dot(self.coefs, x))
+        return [1-prob,prob]
+
+
+    # Predicted classes for an array of instances
     def predict(self,X): 
-        predicted = np.array(len(X))
+        predicted = np.zeros(len(X))
         for i in range(0,len(X)):
-            predicted[i] = np.dot(x, self.coefs)
+            if self.proba(X[i])[0] < self.proba(X[i])[1]:
+                predicted[i] = 1
         return predicted
+
+
+    # Score, the proportion of correct predictions on the array X
+    def score(self, X, y):
+        predicted = self.predict(X)
+        return cr.diffcount(predicted, y)/len(y)
+
+
+    # The loss function -SUM y_i(ln pi) + (1-yi)ln (1-pi)
+    # NOT ACTUALLY NEEDED
+    def E(self,theta):
+        e = 0.0
+        print(len(self.responses))
+        for i in range(0,len(self.responses)):
+            pi = self.sigmoid(np.dot(theta, self.features[i]))
+            yi = self.responses[i]
+            print(pi, yi)
+            e += yi * np.log(pi) + (1-yi) * np.log(1-pi)
+        return -e
